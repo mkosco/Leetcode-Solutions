0639d888382f4d33436852558fc168106130355d
Michael Kosco <kosco.michael@protonmail.com>
2023-02-10 19:25:14 +0100

    Solution added

--- Valid_Palindrome.py
+++ Valid_Palindrome.py
@@ -1,3 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower
+        s = s.lower()
+        clean = ""
+
+        # cleanup the input
+        for c in s:
+            if c.isalnum():
+                clean += c
+        
+        #check for palindrom
+        for i in range(len(clean)//2):
+            left = i
+            right = len(clean) - 1 - i
+            if clean[left] != clean[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
