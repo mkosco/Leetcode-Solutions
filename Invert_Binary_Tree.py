de04309123edadff86aac8c5d26625ab37a9fc90
Michael Kosco <kosco.michael@protonmail.com>
2023-02-10 19:49:25 +0100

    Simple Solution using recursion added

--- Invert_Binary_Tree.py
+++ Invert_Binary_Tree.py
@@ -6,4 +6,10 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        
+        if root is None:
+            return None
+        else:
+            helper = self.invertTree(root.left)
+            root.left = self.invertTree(root.right)
+            root.right = helper
+        return root
