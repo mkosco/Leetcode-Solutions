ef7afe5def74640c40e9e4d22b28111c6911b424
Michael Kosco <kosco.michael@protonmail.com>
2023-02-10 20:02:31 +0100

    Added alternative solution using sorted()

--- Valid_Anagram.py
+++ Valid_Anagram.py
@@ -1,8 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-            
-        for c in s:
-            t = t.replace(c, "", 1)
-        return t == "" 
+        return sorted(s) == sorted(t)
