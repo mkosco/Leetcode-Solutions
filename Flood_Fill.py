20f5403de547dbf63e2e93bfc20197021372a7b4
Michael Kosco <kosco.michael@protonmail.com>
2023-02-11 10:15:20 +0100

    Crude Algorithm implemented, detection of original starting colour still missing

--- Flood_Fill.py
+++ Flood_Fill.py
@@ -1,3 +1,17 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        
+        if image[sr][sc] == color or sr > len(image) - 1 or sc > len(image[0]) - 1 or sr < 0 or sc < 0:
+            return image
+        else:
+            image[sr][sc] = color
+
+            #up
+            self.floodFill(image, sr - 1, sc, color)
+            #down
+            self.floodFill(image, sr + 1, sc, color)
+            #left
+            self.floodFill(image, sr, sc - 1, color)
+            #right
+            self.floodFill(image, sr, sc + 1, color)
+
+            return image
