15f27e0c7d57535ffc904abcd8c561abdf3067e3
Michael Kosco <kosco.michael@protonmail.com>
2023-02-11 16:59:43 +0100

    Recursive Solution added

--- Flood_Fill.py
+++ Flood_Fill.py
@@ -3,15 +3,20 @@ class Solution:
         if image[sr][sc] == color or sr > len(image) - 1 or sc > len(image[0]) - 1 or sr < 0 or sc < 0:
             return image
         else:
+            current = image[sr][sc]
             image[sr][sc] = color
 
             #up
-            self.floodFill(image, sr - 1, sc, color)
+            if not sr - 1 < 0 and image[sr - 1][sc] == current:
+                self.floodFill(image, sr - 1, sc, color)
             #down
-            self.floodFill(image, sr + 1, sc, color)
+            if not sr + 1 > len(image) - 1 and image[sr + 1][sc] == current:
+                self.floodFill(image, sr + 1, sc, color)
             #left
-            self.floodFill(image, sr, sc - 1, color)
+            if not sc - 1 < 0 and image[sr][sc - 1] == current:
+                self.floodFill(image, sr, sc - 1, color)
             #right
-            self.floodFill(image, sr, sc + 1, color)
+            if not sc + 1 > len(image[0]) - 1 and image[sr][sc + 1] == current:
+                self.floodFill(image, sr, sc + 1, color)
 
             return image
