6c66eeb0ff8f6a25ddd5dcd771fa2721ebffd7b7
Michael Kosco <kosco.michael@protonmail.com>
2023-10-18 12:18:05 +0200

    Implement solution for 1431. "Kids With the Greatest Number of Candies". https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/?envType=study-plan-v2&envId=leetcode-75

---
+++ Greates_Num_Candies.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        max_candies = max(candies)
+
+        result = []
+
+        for num_candies in candies:
+            if num_candies + extraCandies >= max_candies:
+                result.append(True)
+            else:
+                result.append(False)
+
+        return result
